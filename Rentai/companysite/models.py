4dc19789921920c4f69f0644b5f24ee02e2461b6
Sekrel_PC <cacopu76@gmail.com>
2025-02-01 03:35:11 +0300

    add game model

--- Rentai/companysite/models.py
+++ Rentai/companysite/models.py
@@ -4,3 +4,10 @@ class New(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    genre = models.CharField(max_length=255)
+    release_date = models.DateTimeField(auto_now_add=True)
