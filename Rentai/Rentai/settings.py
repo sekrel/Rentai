fb7bf2698b89b2f07cb5e9040d8c85908233738a
Sekrel_PC <cacopu76@gmail.com>
2025-02-11 02:33:06 +0300

    add botstrap in project and style setting

--- Rentai/Rentai/settings.py
+++ Rentai/Rentai/settings.py
@@ -132,4 +132,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
